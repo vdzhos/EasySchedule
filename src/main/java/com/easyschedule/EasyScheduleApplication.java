10050f82b11172d1e4b9f42875ce818947cb8b3a
--replace-all <dgosan37@gmail.com>
2023-02-23 01:32:02 +0200

    Project skeleton

---
+++ src/main/java/com/easyschedule/EasyScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.easyschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyScheduleApplication.class, args);
+    }
+
+}
